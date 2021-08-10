e5deae23bbc2077502dee9d930546847874338c6
carokrny <carolynkrny@gmail.com>
2021-08-10 00:04:32 -0400

    Add colors to terminal output

--- main.js
+++ main.js
@@ -1,32 +1,25 @@
 const prompt = require('prompt-sync')({sigint: true});
+const chalk = require('chalk');
 
-const hat = '^';
+const hat = chalk.bgYellowBright(chalk.magentaBright('^'));
 const hole = 'O';
-const fieldCharacter = '░';
-const pathCharacter = '*';
+const fieldCharacter = chalk.whiteBright('░');
+const pathCharacter = chalk.magentaBright('*');
+const pathCharacterCurrent = chalk.bgMagentaBright(chalk.white(('*')));
 
 class Field {
+    // Field contructor 
     constructor(field) {
+        // assign field property to input 2D array
         this.field = field;
+        // assign user starting position to x=0, y=0 (top left)
+        this.x = 0;
+        this.y = 0;
     }
 
-    print() {
-        this.field.forEach(element => {
-            console.log(element.join(' '));
-        });
-    }
-
-    markPath(pos) {
-        this.field[pos.y][pos.x] = pathCharacter;
-    }
-
-    getMarker(pos) {
-        return this.field[pos.y][pos.x];
-    }
-
+    // Generates a random field 
     static generateField(width, height, percentage) {
         const arr = [];
-
         // randomly fill the field with holes that show up percentage of the time
         for (let i = 0; i < height; i++) {
             const row = [];
@@ -41,7 +34,7 @@ class Field {
         }
         
         // assign the path character to the top left corner
-        arr[0][0] = pathCharacter;
+        arr[0][0] = pathCharacterCurrent;
 
         // randomly assign the hat to a location
         const holeRow = Math.floor(Math.random()*height);
@@ -50,113 +43,130 @@ class Field {
 
         return new Field(arr);
     }
-    
-}
 
-class Position {
-    constructor (x, y){
-        this.x = x;
-        this.y = y;
+    // print out the field 
+    print() {
+        this.field.forEach(element => {
+            console.log(chalk.bgGreenBright(element.join(' ')));
+        });
     }
 
-    onField(myField) {
-        return (this.x > -1) && (this.y > -1) && (this.y < myField.field.length) && (this.x < myField.field[0].length);
+    // mark where the user has moved
+    markPath() {
+        this.field[this.y][this.x] = pathCharacter;
     }
-}
 
+    // mark where the user currently is 
+    markPathCurrent() {
+        this.field[this.y][this.x] = pathCharacterCurrent;
+    }
 
-// Helper function to mark positions traveled on field. 
-function getMove(pos) {
-     // User input from terminal to make a move
-    let validMove = false; 
-    while (!validMove) {
-        let move = prompt('Make a move (U, D, L, or R): ');
-        move = move.toUpperCase();
-        switch(move) {
-            // Determine move and change position accordingly.
-            case 'U':
-                pos.y -=1;
-                validMove = true;
-                break;
-            case 'D':
-                pos.y +=1;
-                validMove = true;
-                break;
-            case 'L':
-                pos.x -=1;
-                validMove = true;
-                break;
-            case 'R':
-                pos.x +=1;
-                validMove = true;
-                break;
-            // Repeat loop if input invalid. 
-            default:
-                console.log ('Invalid entry. Please try again.');
+    // get the marker that the move just landed on 
+    getMarker() {
+        return this.field[this.y][this.x];
+    }
+
+    // determine if move is on the field
+    onField() {
+        return (this.x > -1) && (this.y > -1) && (this.y < this.field.length) && (this.x < this.field[0].length);
+    }
+
+    // determine if move is on a hole
+    onHole() {
+        return this.getMarker() === hole;
+    }
+
+    // determine if move is on the hat
+    onHat() {
+        return this.getMarker() === hat;
+    }
+
+    // getMove() retrieves a move from the user via the terminal 
+    getMove() {
+        // mark previous move to normal path character;
+        this.markPath();
+        
+        // receive user input from terminal 
+        // prompt will loop until a valid move is made by the user.
+        let validMove = false; 
+        while (!validMove) {
+            let move = prompt('Make a move (U, D, L, or R): ');
+            move = move.toUpperCase();
+            switch(move) {
+                // Determine move and change position accordingly.
+                case 'U':
+                    this.y -=1;
+                    validMove = true;
+                    break;
+                case 'D':
+                    this.y +=1;
+                    validMove = true;
+                    break;
+                case 'L':
+                    this.x -=1;
+                    validMove = true;
+                    break;
+                case 'R':
+                    this.x +=1;
+                    validMove = true;
+                    break;
+                // Repeat loop if input invalid. 
+                default:
+                    console.log ('Invalid entry. Please try again.');
+            }
+        }
+    }
+
+    // game() runs through one game of Find My Hat
+    game() {
+        // Set up game
+        let playGame = true;
+
+        // Loop through game
+        while(playGame) {
+            this.print();
+            this.getMove();
+            console.log(); // add blank line to visually break up moves
+            if(!this.onField()) { // Determine if move is off board 
+                console.log('Moved off field. You lose.');
+                playGame = false;
+            } else if (this.onHole()) {
+                console.log('Stepped in a hole. You lose.');
+                playGame = false;
+            } else if (this.onHat()) {
+                console.log('Found the hat! You win!');
+                playGame = false;
+            }
+            this.markPathCurrent();
+        }
+    }
+
+    // play() loops explains rules and loops through games until user quits
+    static play() {
+        // explain game rules 
+        console.log('\nWelcome to Find My Hat!');
+        console.log('I\'ve lost my hat and need help finding it. It looks like this: ' + hat);
+        console.log('You are the ' + pathCharacter + ' symbol. Use the keys to navigate to get my hat.');
+        console.log('But don\'t fall down a hole' + hole + 'or off the field!\n');
+        console.log('Press ctrl + c to quit at any time.')
+
+        // loop through playing games until user quits
+        let playing = true; 
+        while(playing) {
+            const randField = Field.generateField(15, 10, 25);
+            randField.game();
+
+            let again = prompt('End of game. Play again? (Y/N): ');
+            again = again.toUpperCase();
+            if (again === 'Y') {
+                console.log('\nCreating a new board!\n')
+            } else {
+                playing = false;
+                console.log('\nBye!\n');
+            }
         }
     }
 }
 
-
-// Run through the game 
-function game(field) {
-    // Set up game pieces
-    field.print();
-    let pos = new Position(0, 0);
-    let hatFound = false;
-
-    // Loop through game
-    while(!hatFound) {
-        // Have user make a move on the field 
-        getMove(pos);
-
-        // Determine if move is off board 
-        if(!pos.onField(field)) {
-            console.log('Moved off field. You lose.');
-            break;
-        }
-
-        // Determine what marker was stepped on 
-        let marker = field.getMarker(pos);
-        if (marker === hole) {
-            console.log('Stepped in a hole. You lose.');
-            break;
-        } else if (marker === hat) {
-            hatFound = true;
-        }
-        field.markPath(pos);
-        console.log(); // add blank line to visually break up moves
-        field.print();
-    }
-    if(hatFound) {
-        console.log('Found the hat! You win!');
-    }
-}
-
-// Run game on repeat until user exits. 
-function play() {
-    console.log('\nWelcome to Find My Hat!');
-    console.log('I\'ve lost my hat and need help finding it. It looks like this: ^');
-    console.log('You are the * symbol. Use the keys to navigate to get my hat.');
-    console.log('But don\'t fall down a hole (O) or off the field!\n');
-    console.log('Press ctrl + c to quit at any time.')
-
-    let playing = true; 
-    while(playing) {
-        const randField = Field.generateField(15, 10, 25);
-        game(randField);
-
-        let again = prompt('End of game. Play again? (Y/N): ');
-        again = again.toUpperCase();
-        if (again === 'Y') {
-            console.log('\nCreating a new board!\n')
-        } else {
-            playing = false;
-            console.log('\nBye!\n');
-        }
-    }
-}
- 
-
-//Test code 
-play();
+ // Play in terminal 
+Field.play();
